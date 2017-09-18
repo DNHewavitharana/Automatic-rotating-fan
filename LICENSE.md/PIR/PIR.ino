09c038c4baffe3f006d93911ea8bba3f414650b1
DNHewavitharana <dilshan.15@cse.mrt.ac.lk>
2017-09-18 17:40:29 +0530

    Add files via upload

---
+++ LICENSE.md/PIR/PIR.ino
@@ -0,0 +1,20 @@
+int led= 4;
+int pir_left= 8;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led,OUTPUT);
+  pinMode(pir_left,INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int x =digitalRead(pir_left);
+  Serial.println(x);
+  if (x==1){
+    digitalWrite(led,HIGH);
+  }else{
+    digitalWrite(led,LOW);
+    }
+}
